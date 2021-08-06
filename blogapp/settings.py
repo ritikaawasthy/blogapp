e1fe4883e7adc1b802e0e856c01d8a3fef05f075
ritikaawasthy <ritika.awasthy@gmail.com>
2021-08-06 10:08:17 +0530

    static settings

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -127,5 +127,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 django_heroku.settings(locals())
